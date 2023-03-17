5d40facea793f77fc6601ea2d0be46394d0ea7fa
Tien <tmtien0103@gmail.com>
2023-03-17 16:45:55 +0700

    fix bug post_property (create)

--- post_property.php
+++ post_property.php
@@ -9,144 +9,142 @@ if(isset($_COOKIE['user_id'])){
    header('location:login.php');
 }
 
-if (isset($POST['post'])) {
+if (isset($_POST['post'])) {
 
     $id = create_unique_id();
-    $property_name = $POST['property_name'];
+    $property_name = $_POST['property_name'];
     $property_name = filter_var($property_name, FILTER_SANITIZE_STRING);
 
-    $price = $POST['price'];
+    $price = $_POST['price'];
     $price = filter_var($price, FILTER_SANITIZE_STRING);
 
-    $deposited = $POST['deposited'];
-    $deposited = filter_var($deposited, FILTER_SANITIZE_STRING);
+    $deposite = $_POST['deposite'];
+    $deposite = filter_var($deposite, FILTER_SANITIZE_STRING);
 
-    $type = $POST['type'];
-    $type = filter_var($type, FILTER_SANITIZE_STRING);
-
-    $address = $POST['address'];
+    $address = $_POST['address'];
     $address = filter_var($address, FILTER_SANITIZE_STRING);
 
-    $furnished = $POST['furnished'];
+    $offer = $_POST['offer'];
+    $offer = filter_var($offer, FILTER_SANITIZE_STRING);
+
+    $type = $_POST['type'];
+    $type = filter_var($type, FILTER_SANITIZE_STRING);
+
+    $status = $_POST['status'];
+    $status = filter_var($status, FILTER_SANITIZE_STRING);
+
+    $furnished = $_POST['furnished'];
     $furnished = filter_var($furnished, FILTER_SANITIZE_STRING);
 
-    $price = $POST['price'];
-    $price = filter_var($price, FILTER_SANITIZE_STRING);
-
-    $bhk = $POST['bhk'];
+    $bhk = $_POST['bhk'];
     $bhk = filter_var($bhk, FILTER_SANITIZE_STRING);
 
-    $bathroom = $POST['bathroom'];
-    $bathroom = filter_var($bathroom, FILTER_SANITIZE_STRING);
-
-    $bedroom = $POST['bedroom'];
+    $bedroom = $_POST['bedroom'];
     $bedroom = filter_var($bedroom, FILTER_SANITIZE_STRING);
 
-    $balcony = $POST['balcony'];
+    $bathroom = $_POST['bathroom'];
+    $bathroom = filter_var($bathroom, FILTER_SANITIZE_STRING);
+
+    $balcony = $_POST['balcony'];
     $balcony = filter_var($balcony, FILTER_SANITIZE_STRING);
 
-    $carpet = $POST['carpet'];
+    $carpet = $_POST['carpet'];
     $carpet = filter_var($carpet, FILTER_SANITIZE_STRING);
 
-    $age = $POST['age'];
+    $age = $_POST['age'];
     $age = filter_var($age, FILTER_SANITIZE_STRING);
 
-    $loan = $POST['loan'];
-    $loan = filter_var($loan, FILTER_SANITIZE_STRING);
-
-    $room_floor = $POST['room_floor'];
+    $total_floors = $_POST['total_floors'];
+    $total_floors = filter_var($total_floors, FILTER_SANITIZE_STRING);
+    
+    $room_floor = $_POST['room_floor'];
     $room_floor = filter_var($room_floor, FILTER_SANITIZE_STRING);
 
-    $description = $POST['description'];
+    $loan = $_POST['loan'];
+    $loan = filter_var($loan, FILTER_SANITIZE_STRING);
+    
+    $description = $_POST['description'];
     $description = filter_var($description, FILTER_SANITIZE_STRING);
 
-    if (isset($POST['list'])) {
-        $list = $_POST['list'];
-        $list = filter_var($list, FILTER_SANITIZE_STRING);
+    if (isset($_POST['lift'])) {
+        $lift = $_POST['lift'];
+        $lift = filter_var($lift, FILTER_SANITIZE_STRING);
     } else {
-        $list = 'no';
+        $lift = 'no';
     }
-    if (isset($POST['security_guard'])) {
+    if (isset($_POST['security_guard'])) {
         $security_guard = $_POST['security_guard'];
         $security_guard = filter_var($security_guard, FILTER_SANITIZE_STRING);
     } else {
         $security_guard = 'no';
     }
-    if (isset($POST['play_ground'])) {
+    if (isset($_POST['play_ground'])) {
         $play_ground = $_POST['play_ground'];
         $play_ground = filter_var($play_ground, FILTER_SANITIZE_STRING);
     } else {
         $play_ground = 'no';
     }
-    if (isset($POST['garden'])) {
+    if (isset($_POST['garden'])) {
         $garden = $_POST['garden'];
         $garden = filter_var($garden, FILTER_SANITIZE_STRING);
     } else {
         $garden = 'no';
     }
-    if (isset($POST['water_supply'])) {
+    if (isset($_POST['water_supply'])) {
         $water_supply = $_POST['water_supply'];
         $water_supply = filter_var($water_supply, FILTER_SANITIZE_STRING);
     } else {
         $water_supply = 'no';
     }
-    if (isset($POST['power_backup'])) {
+    if (isset($_POST['power_backup'])) {
         $power_backup = $_POST['power_backup'];
         $power_backup = filter_var($power_backup, FILTER_SANITIZE_STRING);
     } else {
         $power_backup = 'no';
     }
-    if (isset($POST['parking_area'])) {
+    if (isset($_POST['parking_area'])) {
         $parking_area = $_POST['parking_area'];
         $parking_area = filter_var($parking_area, FILTER_SANITIZE_STRING);
     } else {
         $parking_area = 'no';
     }
-    if (isset($POST['gym'])) {
+    if (isset($_POST['gym'])) {
         $gym = $_POST['gym'];
         $gym = filter_var($gym, FILTER_SANITIZE_STRING);
     } else {
         $gym = 'no';
     }
-    if (isset($POST['shopping_mall'])) {
+    if (isset($_POST['shopping_mall'])) {
         $shopping_mall = $_POST['shopping_mall'];
         $shopping_mall = filter_var($shopping_mall, FILTER_SANITIZE_STRING);
     } else {
         $shopping_mall = 'no';
     }
-    if (isset($POST['hospital'])) {
+    if (isset($_POST['hospital'])) {
         $hospital = $_POST['hospital'];
         $hospital = filter_var($hospital, FILTER_SANITIZE_STRING);
     } else {
         $hospital = 'no';
     }
-    if (isset($POST['school'])) {
+    if (isset($_POST['school'])) {
         $school = $_POST['school'];
         $school = filter_var($school, FILTER_SANITIZE_STRING);
     } else {
         $school = 'no';
     }
-    if (isset($POST['market_area'])) {
+    if (isset($_POST['market_area'])) {
         $market_area = $_POST['market_area'];
         $market_area = filter_var($market_area, FILTER_SANITIZE_STRING);
     } else {
         $market_area = 'no';
     }
 
-    $image_01 = $FILES['image_01']['name'];
-    $image_01 = filter_var($image_01, FILTER_SANITIZE_STRING);
-    $image_01_ext = pathinfo($image_01, PATHINFO_EXTENSION);
-    $rename_image_01 = create_unique_id().'.'.$image_01_ext;
-    $image_01_size = $FILES['image_01']['size'];
-    $image_01_tmp_size = $FILES['image_01']['tmp_size'];
-    $image_01_folder = 'uploaded_files/'.$rename_image_01;
-
-    $image_02 = $FILES['image_02']['name'];
+    $image_02 = $_FILES['image_02']['name'];
     $image_02 = filter_var($image_02, FILTER_SANITIZE_STRING);
     $image_02_ext = pathinfo($image_02, PATHINFO_EXTENSION);
     $rename_image_02 = create_unique_id().'.'.$image_02_ext;
-    $image_02_size = $FILES['image_02']['size'];
-    $image_02_tmp_size = $FILES['image_02']['tmp_size'];
+    $image_02_tmp_name = $_FILES['image_02']['tmp_name'];
+    $image_02_size = $_FILES['image_02']['size'];
     $image_02_folder = 'uploaded_files/'.$rename_image_02;
 
     if (!empty($image_02)) {
@@ -159,12 +157,12 @@ if (isset($POST['post'])) {
         $rename_image_02 = '';
     }
 
-    $image_03 = $FILES['image_03']['name'];
+    $image_03 = $_FILES['image_03']['name'];
     $image_03 = filter_var($image_03, FILTER_SANITIZE_STRING);
     $image_03_ext = pathinfo($image_03, PATHINFO_EXTENSION);
     $rename_image_03 = create_unique_id().'.'.$image_03_ext;
-    $image_03_size = $FILES['image_03']['size'];
-    $image_03_tmp_size = $FILES['image_03']['tmp_size'];
+    $image_03_tmp_name = $_FILES['image_03']['tmp_name'];
+    $image_03_size = $_FILES['image_03']['size'];
     $image_03_folder = 'uploaded_files/'.$rename_image_03;
 
     if (!empty($image_03)) {
@@ -177,12 +175,12 @@ if (isset($POST['post'])) {
         $rename_image_03 = '';
     }
 
-    $image_04 = $FILES['image_04']['name'];
+    $image_04 = $_FILES['image_04']['name'];
     $image_04 = filter_var($image_04, FILTER_SANITIZE_STRING);
     $image_04_ext = pathinfo($image_04, PATHINFO_EXTENSION);
     $rename_image_04 = create_unique_id().'.'.$image_04_ext;
-    $image_04_size = $FILES['image_04']['size'];
-    $image_04_tmp_size = $FILES['image_04']['tmp_size'];
+    $image_04_tmp_name = $_FILES['image_04']['tmp_name'];
+    $image_04_size = $_FILES['image_04']['size'];
     $image_04_folder = 'uploaded_files/'.$rename_image_04;
 
     if (!empty($image_04)) {
@@ -195,30 +193,12 @@ if (isset($POST['post'])) {
         $rename_image_04 = '';
     }
 
-    $image_04 = $FILES['image_04']['name'];
-    $image_04 = filter_var($image_04, FILTER_SANITIZE_STRING);
-    $image_04_ext = pathinfo($image_04, PATHINFO_EXTENSION);
-    $rename_image_04 = create_unique_id().'.'.$image_04_ext;
-    $image_04_size = $FILES['image_04']['size'];
-    $image_04_tmp_size = $FILES['image_04']['tmp_size'];
-    $image_04_folder = 'uploaded_files/'.$rename_image_04;
-
-    if (!empty($image_04)) {
-        if($image_04_size > 2000000) {
-            $warning_msg[] = 'image 04 size is too large';
-        }else {
-            move_uploaded_file($image_04_tmp_name, $image_04_folder);
-        }
-    } else {
-        $rename_image_04 = '';
-    }
-
-    $image_05 = $FILES['image_05']['name'];
+    $image_05 = $_FILES['image_05']['name'];
     $image_05 = filter_var($image_05, FILTER_SANITIZE_STRING);
     $image_05_ext = pathinfo($image_05, PATHINFO_EXTENSION);
     $rename_image_05 = create_unique_id().'.'.$image_05_ext;
-    $image_05_size = $FILES['image_05']['size'];
-    $image_05_tmp_size = $FILES['image_05']['tmp_size'];
+    $image_05_tmp_name = $_FILES['image_05']['tmp_name'];
+    $image_05_size = $_FILES['image_05']['size'];
     $image_05_folder = 'uploaded_files/'.$rename_image_05;
 
     if (!empty($image_05)) {
@@ -231,13 +211,21 @@ if (isset($POST['post'])) {
         $rename_image_05 = '';
     }
 
+    $image_01 = $_FILES['image_01']['name'];
+    $image_01 = filter_var($image_01, FILTER_SANITIZE_STRING);
+    $image_01_ext = pathinfo($image_01, PATHINFO_EXTENSION);
+    $rename_image_01 = create_unique_id().'.'.$image_01_ext;
+    $image_01_tmp_name = $_FILES['image_01']['tmp_name'];
+    $image_01_size = $_FILES['image_01']['size'];
+    $image_01_folder = 'uploaded_files/'.$rename_image_01;
+
     if($image_01_size > 2000000) {
         $warning_msg[] = 'image 01 size is too large';
     }else {
-        $post_property = $conn -> prepare("INSERT INTO `property`(id, user_id, property_name, address, price, type, offer, status, furnished, bhk, deposite, bedroom, bathroom, balcony, carpet, age, total_floors, room_floor, loan, lift, security_guard, play_ground, garden, water_supply, power_backup, parking_area, gym, shopping_mall, hospital, school, market_area, image_01, image_02, image_03, image_04, image_05, description) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)");
-        $post_property -> execute([$id, $user_id, $property_name, $address, $price, $type, $offer, $status, $furnished, $bhk, $deposite, $bedroom, $bathroom, $balcony, $carpet, $age, $total_floors, $room_floor, $loan, $lift, $security_guard, $play_ground, $garden, $water_supply, $power_backup, $parking_area, $gym, $shopping_mall, $hospital, $school, $market_area, $rename_image_01, $rename_image_02, $rename_image_03, $rename_image_04, $rename_image_05, $description]);
+        $post_property = $conn->prepare("INSERT INTO `property`(id, user_id, property_name, address, price, type, offer, status, furnished, bhk, deposite, bedroom, bathroom, balcony, carpet, age, total_floors, room_floor, loan, lift, security_guard, play_ground, garden, water_supply, power_backup, parking_area, gym, shopping_mall, hospital, school, market_area, image_01, image_02, image_03, image_04, image_05, description) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)");
+        $post_property->execute([$id, $user_id, $property_name, $address, $price, $type, $offer, $status, $furnished, $bhk, $deposite, $bedroom, $bathroom, $balcony, $carpet, $age, $total_floors, $room_floor, $loan, $lift, $security_guard, $play_ground, $garden, $water_supply, $power_backup, $parking_area, $gym, $shopping_mall, $hospital, $school, $market_area, $rename_image_01, $rename_image_02, $rename_image_03, $rename_image_04, $rename_image_05, $description]);
         move_uploaded_file($image_01_tmp_name, $image_01_folder);
-        $success_msg[] = 'property posted!';
+        $success_msg[] = 'property posted successfully!';
     }
 }
 
@@ -291,7 +279,7 @@ include 'components/save_send.php';
             </div>
             <div class="box">
                 <p>Offer Type<span>*</span></p>
-                <select name="offer" class="input" required>
+                <select name="offer" required class="input">
                     <option value="sale">sale</option>
                     <option value="resale">resale</option>
                     <option value="rent">rent</option>
@@ -299,30 +287,30 @@ include 'components/save_send.php';
             </div>
             <div class="box">
                 <p>Property Type<span>*</span></p>
-                <select name="type" class="input" required>
-                    <option value="sale">sale</option>
+                <select name="type" required class="input">
+                    <option value="flat">flat</option>
                     <option value="house">house</option>
                     <option value="shop">shop</option>
                 </select>
             </div>
             <div class="box">
                 <p>Property Status<span>*</span></p>
-                <select name="status" class="input" required>
+                <select name="status" required class="input">
                     <option value="ready to move">ready to move</option>
                     <option value="under construction">under construction</option>
                 </select>
             </div>
             <div class="box">
                 <p>Furnished Status<span>*</span></p>
-                <select name="furnished" class="input" required>
-                    <option value="unfurnished">unfurnished</option>
-                    <option value="semi-furnished">semi-furnished</option>
+                <select name="furnished" required class="input">
                     <option value="furnished">furnished</option>
+                    <option value="semi-furnished">semi-furnished</option>
+                    <option value="unfurnished">unfurnished</option>
                 </select>
             </div>
             <div class="box">
                 <p>How many BHK<span>*</span></p>
-                <select name="bhk" class="input" required>
+                <select name="bhk" required class="input">
                     <option value="1">1 BHK</option>
                     <option value="2">2 BHK</option>
                     <option value="3">3 BHK</option>
@@ -336,7 +324,7 @@ include 'components/save_send.php';
             </div>
             <div class="box">
                 <p>How many bedrooms<span>*</span></p>
-                <select name="bedroom" class="input" required>
+                <select name="bedroom" required class="input">
                     <option value="1">1 bedroom</option>
                     <option value="2">2 bedrooms</option>
                     <option value="3">3 bedrooms</option>
@@ -350,7 +338,7 @@ include 'components/save_send.php';
             </div>
             <div class="box">
                 <p>How many bathrooms<span>*</span></p>
-                <select name="bathroom" class="input" required>
+                <select name="bathroom" required class="input">
                     <option value="1">1 bathroom</option>
                     <option value="2">2 bathrooms</option>
                     <option value="3">3 bathrooms</option>
@@ -367,18 +355,18 @@ include 'components/save_send.php';
                 <select name="balcony" class="input" required>
                     <option value="0">0 balcony</option>
                     <option value="1">1 balcony</option>
-                    <option value="2">2 balconys</option>
-                    <option value="3">3 balconys</option>
-                    <option value="4">4 balconys</option>
-                    <option value="5">5 balconys</option>
-                    <option value="6">6 balconys</option>
-                    <option value="7">7 balconys</option>
-                    <option value="8">8 balconys</option>
-                    <option value="9">9 balconys</option>
+                    <option value="2">2 balcony</option>
+                    <option value="3">3 balcony</option>
+                    <option value="4">4 balcony</option>
+                    <option value="5">5 balcony</option>
+                    <option value="6">6 balcony</option>
+                    <option value="7">7 balcony</option>
+                    <option value="8">8 balcony</option>
+                    <option value="9">9 balcony</option>
                 </select>
             </div>
             <div class="box">
-                <p>Carpet Area (sqft)<span>*</span></p>
+                <p>Carpet Area<span>*</span></p>
                 <input type="number" name="carpet" maxlength="10" min="0" max="9999999999" required placeholder="How May Squarefits"class="input">
             </div>
             <div class="box">
@@ -391,11 +379,11 @@ include 'components/save_send.php';
             </div>
             <div class="box">
                 <p>Room Floor<span>*</span></p>
-                <input type="number" name="room_fllor" maxlength="2" min="0" max="99" required placeholder="Property floor number"class="input">
+                <input type="number" name="room_floor" maxlength="2" min="0" max="99" required placeholder="Property floor number"class="input">
             </div>
             <div class="box">
                 <p>loan<span>*</span></p>
-                <select name="status" class="input" required>
+                <select name="loan" required class="input">
                     <option value="available">available</option>
                     <option value="not available">not available</option>
                 </select>
@@ -410,12 +398,12 @@ include 'components/save_send.php';
                 <p><input type="checkbox" name="lift" value="yes"/>Lifts</p>
                 <p><input type="checkbox" name="security_guard" value="yes"/>Security Guard</p>
                 <p><input type="checkbox" name="play_ground" value="yes"/>Play Ground</p>
-                <p><input type="checkbox" name="Garden" value="yes"/>Garden</p>
+                <p><input type="checkbox" name="garden" value="yes"/>Garden</p>
                 <p><input type="checkbox" name="water_supply" value="yes"/>Water Supply</p>
                 <p><input type="checkbox" name="power_backup" value="yes"/>Power Backup</p>
             </div>
             <div class="box">
-                <p><input type="checkbox" name="Parking_area" value="yes"/>Parking_area</p>
+                <p><input type="checkbox" name="parking_area" value="yes"/>Parking_area</p>
                 <p><input type="checkbox" name="gym" value="yes"/>gym</p>
                 <p><input type="checkbox" name="shopping_mall" value="yes"/>shopping_mall</p>
                 <p><input type="checkbox" name="hospital" value="yes"/>hospital</p>
