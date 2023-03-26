d3d700d7b4bed4a0a750dcc8d60bb06360b48825
Tien <tmtien0103@gmail.com>
2023-03-26 09:53:17 +0700

    my listing read

--- my_listings.php
+++ my_listings.php
@@ -34,7 +34,70 @@ include 'components/save_send.php';
     <?php include 'components/user_header.php'; ?>
     <!-- header end -->
 
+<section class="my-listings">
 
+    <h1 class="heading">My Listings</h1>
+
+    <div class="box-container">
+
+    <?php
+        $select_listings = $conn->prepare("SELECT * FROM `property` WHERE user_id = ? ORDER BY date DESC");
+        $select_listings->execute([$user_id]);
+        if($select_listings->rowCount() > 0) {
+            while($fetch_listing = $select_listings->fetch(PDO::FETCH_ASSOC)) {
+                $listing_id = $fetch_listing['id'];
+
+                if(!empty($fetch_listing['image_02'])) {
+                    $image_02 = 1;
+                }else{
+                    $image_02 = 0;
+                }
+                if(!empty($fetch_listing['image_03'])) {
+                    $image_03 = 1;
+                }else{
+                    $image_03 = 0;
+                }
+                if(!empty($fetch_listing['image_04'])) {
+                    $image_04 = 1;
+                }else{
+                    $image_04 = 0;
+                }
+                if(!empty($fetch_listing['image_05'])) {
+                    $image_05 = 1;
+                }else{
+                    $image_05 = 0;
+                }
+            
+            $total_images = (1 + $image_02 + $image_03 + $image_04 + $image_05);      
+    ?>
+    
+    
+    <form action="" method="POST" class="box">
+        <input type="hidden" name="property_id" value="<?= $listing_id; ?>" >
+        <div class="thumb">
+            <p><i class="far fa-image"></i><span><?= $total_images; ?></span></p>
+            <img src="uploaded_files/<?= $fetch_listing['image_01'] ?>" alt="">
+        </div>
+        <p class="price"><i class="fas fa-vietnam-dong-sign"></i><?= $fetch_listing['price']; ?></p>
+        <h3 class="name"><?= $fetch_listing['property_name']; ?></h3>
+        <p class="address"><i class="fas fa-map-market-alt"></i><?= $fetch_listing['address']; ?></p>
+        <div class="flex-btn">
+            <a href="update_property.php?get_id=<?= $listing_id; ?>" class="btn">update</a>
+            <input type="submit" value="delete" name="delete" class="btn" onclick="return confirm('delete this listing?');">
+        </div>
+        <a href="view_property.php?get_id=<?= $listing_id; ?>" class="btn">View Property</a>
+    </form>
+
+    <?php
+            }
+        }else {
+            echo '<p class="empty">No listings found!</p>';
+        }
+    ?>
+        
+    </div>
+
+</section>
 
 
 
